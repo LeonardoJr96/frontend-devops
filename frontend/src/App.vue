9a6d1538499e25bbd8180bd7425ed0c39f213287
Leonardo de Almeida Junior <leo70x7000@gmail.com>
2025-07-15 11:21:31 -0300

    first commit

---
+++ frontend/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h1>Tasks</h1>
+    <ul>
+      <li v-for="task in tasks" :key="task.id">
+        {{ task.title }} - <strong>{{ task.done ? 'Done' : 'Pending' }}</strong>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script setup>
+import { ref, onMounted } from 'vue'
+import axios from 'axios'
+
+const tasks = ref([])
+
+onMounted(async () => {
+  const response = await axios.get('http://localhost:8000/api/tasks/')
+  tasks.value = response.data
+})
+</script>
